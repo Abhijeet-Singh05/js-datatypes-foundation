373bf2cfddc5bbb7e199c32b518b8fb832e47e07
Abhijeet Singh <singh.abhijeet9342@gmail.com>
2026-07-19 14:55:44 +0530

    pincode-checker

--- src/10-pincode-checker.js
+++ src/10-pincode-checker.js
@@ -59,20 +59,33 @@
  */
 export function getDataType(value) {
   // Your code here
+  if (value === null) return "null";
+  if (Array.isArray(value)) return "array";
+
+  return typeof value;
 }
 
 export function isValidParcelWeight(weight) {
   // Your code here
+
+  if (typeof weight === "number" && Number.isFinite(weight) && weight > 0)
+    return true;
+  return false;
 }
 
 export function isWholeNumber(value) {
   // Your code here
+  if (Number.isInteger(value)) return true;
+  return false;
 }
 
 export function isNotANumber(value) {
   // Your code here
+  return Number.isNaN(value);
 }
 
 export function isTruthy(value) {
   // Your code here
+
+  return value ? true : false;
 }
